ba7e64b188e2bb2b72df0bd60a6749eeed0b6afa
unknown <sample@example.com>
2024-09-27 12:19:11 +0900

    I/O, imports

--- week05/main.go
+++ week05/main.go
@@ -1,11 +1,30 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"math"
+	"strings"
+)
 
-func main() {
-	var i int = 55
+func imports() {
+	// var f float32
+	fmt.Println(math.Floor(2.75))
+	fmt.Println(math.Ceil(2.1))
+	fmt.Printf("%s\n", strings.Title("head fist go"))
+}
+
+func I_O() {
+	i := 55
+	//var i int = 55
+	// var i int
+	// i = 55
+
 	fmt.Printf("value i : %d\n", i)
 	fmt.Print("value i : ", i, "\n")
 	fmt.Println("value i :", i)
+}
 
+func main() {
+	// I_O()
+	imports()
 }
