a76662bdb4e4e0350be1115593729cc894394866
unknown <sample@example.com>
2024-09-27 12:24:15 +0900

    I/O, imports, TypeOf

--- week05/main.go
+++ week05/main.go
@@ -3,18 +3,20 @@ package main
 import (
 	"fmt"
 	"math"
+	"reflect"
 	"strings"
 )
 
 func imports() {
-	// var f float32
 	fmt.Println(math.Floor(2.75))
 	fmt.Println(math.Ceil(2.1))
 	fmt.Printf("%s\n", strings.Title("head fist go"))
 }
 
 func I_O() {
-	i := 55
+	var f float32 = 4.3
+	i := 55   //int
+	j := 55.0 //float64 == double
 	//var i int = 55
 	// var i int
 	// i = 55
@@ -22,9 +24,11 @@ func I_O() {
 	fmt.Printf("value i : %d\n", i)
 	fmt.Print("value i : ", i, "\n")
 	fmt.Println("value i :", i)
+	fmt.Println(reflect.TypeOf(f))
+	fmt.Println(reflect.TypeOf(j))
 }
 
 func main() {
-	// I_O()
-	imports()
+	I_O()
+	// imports()
 }
