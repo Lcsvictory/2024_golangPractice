e52f338fd0207b9aaf73085ec6ff01ec00e3a779
unknown <sample@example.com>
2024-10-04 10:40:40 +0900

    Conversion(형변환)내용추가

--- week06/main.go
+++ week06/main.go
@@ -7,12 +7,12 @@ import (
 
 func main() {
 	i := 13
-	t := 12
+	var t float64 = 12.8
 	f := 12.9 //int(12.9) == 12
 	fmt.Printf("value i :%d,  value F : %f\n", i, f)
 	fmt.Printf("%d * %f = %.02f\n", i, f, float64(i)*f) //형변환
 	fmt.Printf("%d * %f = %d\n", i, f, i*int(f))        //형변환 float -> int시 Floor적용.
 	// fmt.Printf("%d * %f = %.02f\n", i, f, i*f) # int * float = error
 	fmt.Println((reflect.TypeOf(i)))
-	fmt.Println(t == int(f))
+	fmt.Println(reflect.TypeOf(i), reflect.TypeOf(t))
 }
