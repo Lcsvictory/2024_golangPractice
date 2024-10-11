381aa1038fea170e3fe8679d620fd407415bf3a2
unknown <sample@example.com>
2024-10-11 11:53:58 +0900

    ParseInt, ParseFloat

--- week07/main.go
+++ week07/main.go
@@ -1,7 +1,32 @@
 package main
 
+import (
+	"bufio"
+	"fmt"
+	"log"
+	"os"
+	"strconv"
+	"strings"
+)
+
 func main() {
-	// var int int = 1 //예약어를 변수로 사용하면 안된다.
-	// var count int
-	// fmt.Println(count)
+	fmt.Print("your score : ")
+	reader := bufio.NewReader(os.Stdin)
+	i, err := reader.ReadString('\n')
+	if err != nil {
+		log.Fatal(err)
+	}
+	i = strings.TrimSpace(i)
+	score, _ := strconv.ParseInt(i, 16, 32) // 0~36진수 까지 가능. 32비트이냐 64비트이냐.
+	// score, _ := strconv.ParseFloat(i, 32)  //32비트냐 64비트냐.
+	fmt.Printf("%d", score)
+
+	if score >= 60 {
+		fmt.Println("A")
+	} else {
+		fmt.Println("BCDF")
+	}
+
 }
+
+// 16 * 4 + 1
