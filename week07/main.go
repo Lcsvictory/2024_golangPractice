81f2351671f711f29081698cd7364145b8ac7c8d
unknown <sample@example.com>
2024-10-11 11:26:19 +0900

    error, ReadString

--- week07/main.go
+++ week07/main.go
@@ -3,6 +3,7 @@ package main
 import (
 	"bufio"
 	"fmt"
+	"log"
 	"os"
 )
 
@@ -10,6 +11,10 @@ func main() {
 	fmt.Print("your name: ")
 	r := bufio.NewReader(os.Stdin)
 	// i, err := r.ReadString('\n')
-	name, _ := r.ReadString('\n') //\n직전까지 읽어옴.
-	fmt.Printf("Welcome %s.", name)
+	name, err := r.ReadString('\n') //\n포함하여 읽어옴. \n 포함하지 않고 읽고 싶다.
+	if err != nil {                 //(nil = 에러가 없다.) 즉 에러가 있다면.
+		log.Fatal(err)
+	} else {
+		fmt.Printf("Welcome %s", name)
+	}
 }
