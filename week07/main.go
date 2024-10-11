e500698acecf082b18f16dbcae5dfa4b1b69b9e5
unknown <sample@example.com>
2024-10-11 11:33:42 +0900

    Shadow(예약어 덮어쓰기)

--- week07/main.go
+++ week07/main.go
@@ -1,20 +1,7 @@
 package main
 
-import (
-	"bufio"
-	"fmt"
-	"log"
-	"os"
-)
-
 func main() {
-	fmt.Print("your name: ")
-	r := bufio.NewReader(os.Stdin)
-	// i, err := r.ReadString('\n')
-	name, err := r.ReadString('\n') //\n포함하여 읽어옴. \n 포함하지 않고 읽고 싶다.
-	if err != nil {                 //(nil = 에러가 없다.) 즉 에러가 있다면.
-		log.Fatal(err)
-	} else {
-		fmt.Printf("Welcome %s", name)
-	}
+	// var int int = 1 //예약어를 변수로 사용하면 안된다.
+	// var count int
+	// fmt.Println(count)
 }
