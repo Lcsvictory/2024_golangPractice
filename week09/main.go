de21fcfe7894b01d601a308342b4ae7003e1835f
unknown <sample@example.com>
2024-10-25 10:58:49 +0900

    for문 활용법

--- week09/main.go
+++ week09/main.go
@@ -1,41 +1,23 @@
 package main
 
 import (
-	"bufio"
 	"fmt"
-	"log"
-	"math/rand"
-	"os"
-	"strconv"
-	"strings"
-	"time"
 )
 
 func main() {
-	second := time.Now().Unix()
-	rand.Seed(second)
-	answer := rand.Intn(100) + 1 //0 ~ n-1
-	fmt.Printf("%d\n", answer)
-
-	fmt.Print("정답은? : ")
-	reader := bufio.NewReader(os.Stdin)
-	i, err := reader.ReadString('\n')
-	if err != nil {
-		log.Fatal(err)
+	u := 0
+	for {
+		u++
+		fmt.Println(u)
+		if u == 3 {
+			break
+		}
 	}
-	i = strings.TrimSpace(i)
-	guess, err := strconv.Atoi(i)
-	if err != nil {
-		log.Fatal(err)
-	}
-	fmt.Println(guess)
 
-	if answer == guess {
-		fmt.Println("정답입니다.")
-	} else if answer > guess {
-		fmt.Println("숫자가 작습니다. LOW")
-	} else {
-		fmt.Println("숫자가 큽니다. HIGH")
+	t := 0
+	for t < 3 {
+		t++
+		fmt.Println(t)
 	}
 
 }
