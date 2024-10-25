e4d14ea475f477c4a1ed5d94b404d93775acd90c
unknown <sample@example.com>
2024-10-25 11:11:29 +0900

    랜덤 숫자 맞추기 for문 추가

--- week09/main.go
+++ week09/main.go
@@ -1,23 +1,43 @@
 package main
 
 import (
+	"bufio"
 	"fmt"
+	"log"
+	"math/rand"
+	"os"
+	"strconv"
+	"strings"
+	"time"
 )
 
 func main() {
-	u := 0
-	for {
-		u++
-		fmt.Println(u)
-		if u == 3 {
-			break
-		}
-	}
+	second := time.Now().Unix()
+	rand.Seed(second)
+	answer := rand.Intn(6) + 1 //0 ~ n-1
+	fmt.Printf("%d\n", answer)
 
-	t := 0
-	for t < 3 {
-		t++
-		fmt.Println(t)
+	for count := 3; count > 0; count-- {
+		fmt.Printf("남은 기회 : %d회, 정답은? : ", count)
+		reader := bufio.NewReader(os.Stdin)
+		i, err := reader.ReadString('\n')
+		if err != nil {
+			log.Fatal(err)
+		}
+		i = strings.TrimSpace(i)
+		guess, err := strconv.Atoi(i)
+		if err != nil {
+			log.Fatal(err)
+		}
+		// fmt.Println(guess)
+
+		if answer == guess {
+			fmt.Println("정답입니다.")
+		} else if answer > guess {
+			fmt.Println("숫자가 작습니다. LOW")
+		} else {
+			fmt.Println("숫자가 큽니다. HIGH")
+		}
 	}
 
 }
