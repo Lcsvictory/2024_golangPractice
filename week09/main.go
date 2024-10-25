b1075c18c62f48d43d63088e813a45efbb77f6f8
unknown <sample@example.com>
2024-10-25 11:28:46 +0900

    랜덤 숫자 맞추기 최종

--- week09/main.go
+++ week09/main.go
@@ -15,8 +15,8 @@ func main() {
 	second := time.Now().Unix()
 	rand.Seed(second)
 	answer := rand.Intn(6) + 1 //0 ~ n-1
-	fmt.Printf("%d\n", answer)
-
+	// fmt.Printf("%d\n", answer)
+	win := false
 	for count := 3; count > 0; count-- {
 		fmt.Printf("남은 기회 : %d회, 정답은? : ", count)
 		reader := bufio.NewReader(os.Stdin)
@@ -33,11 +33,18 @@ func main() {
 
 		if answer == guess {
 			fmt.Println("정답입니다.")
+			win = true
+			break
 		} else if answer > guess {
 			fmt.Println("숫자가 작습니다. LOW")
 		} else {
 			fmt.Println("숫자가 큽니다. HIGH")
 		}
+	}
+	if win {
+		fmt.Printf("You Win! 정답 : %d\n", answer)
+	} else {
+		fmt.Printf("You Lose.. 정답 : %d\n", answer)
 	}
 
 }
