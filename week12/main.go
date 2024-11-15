ae8f8a6a4300677ecd7ce0e6ce6ed1a1bd0c9d9b
unknown <sample@example.com>
2024-11-15 10:52:35 +0900

    for문, len(), %v포맷

--- week12/main.go
+++ week12/main.go
@@ -22,11 +22,15 @@ func main() {
 		time.Unix(1244444401, 0), //줄바꿈이 있다면 마지막 요소에 ,는 필수
 	}
 
-	dates1 := [3]time.Time{
-		time.Unix(0, 0),
-		time.Unix(1, 0),
-		time.Unix(1947920001, 0)} //줄바꿈 없으면 없어도 됨.
+	// dates1 := [3]time.Time{
+	// 	time.Unix(0, 0),
+	// 	time.Unix(1, 0),
+	// 	time.Unix(1947920001, 0)} //줄바꿈 없으면 없어도 됨.
 
-	fmt.Println(dates[0], dates[1], dates[2])
-	fmt.Println(dates1[0], dates1[1], dates1[2])
+	// fmt.Println(dates[0], dates[1], dates[2])
+	// fmt.Printf("%#v\n", dates1) //literal 형태 그대로 출력.
+
+	for i := 0; i < len(dates); i++ {
+		fmt.Printf("%d, %v\n", i, dates[i])
+	}
 }
