48bda6b63a343f82049c6a1e9131d8b5a9daa3a2
lcsvictory <yuko135z@gmail.com>
2024-11-22 12:23:14 +0900

    MAP

--- week13/main.go
+++ week13/main.go
@@ -2,27 +2,18 @@ package main
 
 import (
 	"fmt"
-	"os"
-	"reflect"
 )
 
-// func test(names string) { //단일매개변수 arguments
-// fmt.Println(names)
-// }
-
-func test(names ...string) { //가변매개변수 variable arguments
-	fmt.Println(names, reflect.TypeOf(names))
-}
-
 func main() {
-	singer := os.Args[1:] //$0 실행파일경로 $1~... 공백으로 분리된 매개변수들
-	// 실행시킬때 커맨드 창에서 입력받은 매개변수들.
-	fmt.Println(singer)
-	fmt.Printf("%T\n", singer[1]) // TypeOf()
 
-	singer = append(singer, "IU", "BTS")
-	fmt.Println(singer, len(singer))
+	//MAPS
+	var myMaps map[string]int = make(map[string]int)
 
-	test("you", "are")
-	test()
+	myMap := map[string]int{
+		"na":  1,
+		"lee": 2,
+	}
+
+	myMaps["kim"] = 10
+	fmt.Println(myMaps, myMap)
 }
