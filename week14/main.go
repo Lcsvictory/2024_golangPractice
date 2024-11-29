3c7eca6cd687170c57b7317b16ae18ec1d0e7bdb
Lcsvictory <yuko135z@gmail.com>
2024-11-29 11:53:44 +0900

    struct 연습(매표소프로그램)

--- week14/main.go
+++ week14/main.go
@@ -2,28 +2,41 @@ package main
 
 import "fmt"
 
-type student struct {
-	id   int
-	name string
-	gpa  float32
+type visitor struct {
 	age  int
+	cost int
+}
+
+func claculateCost(visitors []visitor) int {
+	totalCost := 0
+	for _, value := range visitors {
+		totalCost += value.cost
+	}
+	return totalCost
 }
 
 func main() {
-	var student1 student
-	student1.id = 20241100
-	student1.name = "Kim inha"
-	student1.gpa = 4.5
-	student1.age = 20
-	fmt.Printf("%#v\n", student1.id)
+	var numVisitor int
 
-	var student2 student
-	student2.id = 20242200
-	student2.name = "Kim inha"
-	student2.gpa = 4.5
-	student2.age = 20
-	fmt.Printf("%#v\n", student2.name)
-	// var school struct { 구조체는 타입이 아니다. 클래스도 아니다.
-	// 	student stu
-	// }
+	fmt.Print("방문 인원은 몇명인가? : ")
+	fmt.Scanln(&numVisitor)
+
+	var visitors []visitor
+	visitors = make([]visitor, numVisitor)
+
+	for i := 0; i < numVisitor; i++ {
+		var age int
+		fmt.Print("나이를 입력하세요. : ")
+		fmt.Scanln(&age)
+		switch {
+		case age < 12:
+			visitors[i] = visitor{age: age, cost: 5000}
+		case age >= 12 && age < 65:
+			visitors[i] = visitor{age: age, cost: 10000}
+		default:
+			visitors[i] = visitor{age: age, cost: 7000}
+		}
+	}
+
+	fmt.Printf("Total price is %dwon.\n", claculateCost(visitors))
 }
